b95c9ce31ed866d8fed8128a6256eb55ee24de9b
umar-qureshi2 <umar.mcs@gmail.com>
2018-03-19 17:05:40 +0400

    my solution for super reduced string and test project

---
+++ FizzBuzz.Tests/SuperReducedStringTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using FizzBuzz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Tests
+{
+    [TestFixture()]
+    public class SuperReducedStringTests
+    {
+        [Test()]
+        [TestCase("abba", "Empty String")]
+        [TestCase("aa", "Empty String")]
+        [TestCase("aaabccddd", "abd")]
+        public void super_reduced_stringTest(string reduceMe, string reducedMe)
+        {
+            //setup
+            var result = SuperReducedString.SuperReduceString(reduceMe);
+            //assertion
+            Assert.AreEqual(reducedMe, result);
+        }
+    }
+}
---
+++ FizzBuzz/SuperReducedString.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class SuperReducedString
+    {
+        public static string SuperReduceString(string s)
+        {
+            Stack<char> characterStack = new Stack<char>();
+            if (s.Length > 0)
+            {
+                characterStack.Push(s[0]);
+            }
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                var peek = (char)0;
+                if (characterStack.Count > 0)
+                {
+                    peek = characterStack.Peek();
+                }
+
+                if (peek == s[i])
+                {
+                    characterStack.Pop();
+                }
+                else
+                {
+                    characterStack.Push(s[i]);
+                }
+            }
+
+            if (characterStack.Count == 0)
+            {
+                return "Empty String";
+            }
+            return string.Concat(characterStack.Reverse().ToList());
+        }
+    }
+}
